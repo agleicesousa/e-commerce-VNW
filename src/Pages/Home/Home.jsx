2f8b01073d9c10e42f313ae4949dbefc82f31d3f
agleicesousa <aleicesousa98@gmail.com>
2025-01-20 15:00:06 -0300

    feat: exemplo do uso do map

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,23 @@
 export default function Home() {
+    
+    const tarefas = [
+        'Aprender map()',
+        'Prestar atenção na aula',
+        'Interagir na aula',
+        'Praticar bastante',
+        'Beber bastante, água'
+    ]
+
     return (
         <>
-            <h1>Home</h1>
+            <main>
+                <h1>Minhas terefas</h1>
+                <ul>
+                    {tarefas.map( (item) => (
+                        <li key={item}>{item}</li>
+                    ))}
+                </ul>
+            </main>
         </>
     )
 }
