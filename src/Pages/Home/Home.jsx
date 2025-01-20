07859c842fba3d6830324d37006b773c1e18b619
agleicesousa <aleicesousa98@gmail.com>
2025-01-20 15:35:00 -0300

    feat: usando map no array de listagem

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,23 +1,51 @@
 export default function Home() {
-    
     const tarefas = [
-        'Aprender map()',
         'Prestar atenção na aula',
         'Interagir na aula',
         'Praticar bastante',
         'Beber bastante, água'
-    ]
-
+    ];
+    
+    const comidas = [
+        {
+            nome: 'Lasanha',
+            imagem: 'https://renata.com.br/images/receitas/141/renata-imagem-receitas-lasanha-a-bolonhesa-share.jpg',
+            alt: 'Lazanha com bastante queijo em cima de um prato', 
+            preco: 10.00
+        },
+        {
+            nome: 'Tacacá',
+            imagem: 'https://acozinhabrasileira.com.br/wp-content/uploads/2021/09/receita-de-tacaca.jpg',
+            alt: 'Tacacá em uma tigela com camarão em cima',
+            preco: 50.00
+        },
+        {
+            nome: 'Açaí',
+            imagem: 'https://snowfruit.com.br/wp-content/uploads/2021/08/Quais_s%C3%A3o_os_melhores_acompanhamentos_para_a%C3%A7ai.png',
+            alt: 'Açaí com bastante variedas de acompanhamentos',
+            preco: 14.00
+        }
+    ];
+    
     return (
         <>
-            <main>
-                <h1>Minhas terefas</h1>
-                <ul>
-                    {tarefas.map( (item) => (
-                        <li key={item}>{item}</li>
-                    ))}
-                </ul>
-            </main>
+            <h1>Minhas tarefas</h1>
+            <ul>
+                {tarefas.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
+    
+            <section>
+                {comidas.map((item, index) => (
+                    <article key={index}>
+                        <h2>{item.nome}</h2>
+                        {item.imagem && <img src={item.imagem} alt={item.alt} />}
+                        {item.preco && <p>{item.preco.toFixed(2)}</p>}
+                    </article>
+                ))}
+            </section>
         </>
-    )
+    );    
 }
+
